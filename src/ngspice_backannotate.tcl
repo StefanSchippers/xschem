8cb3172f42acbe3ba0d97dc4e01701094a72b3f6
Stefan Frederik <stefan.schippers@gmail.com>
2020-12-29 15:42:45 +0100

    remove dbg msg

--- src/ngspice_backannotate.tcl
+++ src/ngspice_backannotate.tcl
@@ -121,7 +121,7 @@ proc ngspice::get_current {n} {
     set n @$n
   }
   set n i($n)
-  puts "ngspice::get_current --> $n"
+  # puts "ngspice::get_current --> $n"
   set err [catch {set ::ngspice::ngspice_data($n)} res]
   if { $err } {
     set res {?}
