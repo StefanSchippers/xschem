242523cb9fb48fc1fdc3e4a10ef7ad11b04094bb
Stefan Frederik <stefan.schippers@gmail.com>
2021-06-16 07:15:31 +0200

    typo fix

--- src/vhdl_netlist.c
+++ src/vhdl_netlist.c
@@ -348,7 +348,7 @@ void global_vhdl_netlist(int global)  /* netlister driver */
    {
     if( strcmp(get_tok_value(xctx->sym[i].prop_ptr,"vhdl_ignore",0),"true")==0 ) continue;
     if(!xctx->sym[i].type) continue;
-    my_strdup(1242, &abs_path, abs_sym_path(xctx->sym[j].name, ""));
+    my_strdup(1242, &abs_path, abs_sym_path(xctx->sym[i].name, ""));
     if(strcmp(xctx->sym[i].type,"subcircuit")==0 && check_lib(1, abs_path))
     {
       /* xctx->sym can be SCH or SYM, use hash to avoid writing duplicate subckt */
