abc88e881972fad798f3ffcc716a88de64bd8e7a
stefan schippers <stefan.schippers@gmail.com>
2025-01-03 13:15:30 +0100

    Revert dirty hack to make a click on an open menu close it, still some issues

--- src/xschem.tcl
+++ src/xschem.tcl
@@ -8830,17 +8830,18 @@ if { [info exists has_x]} {
 
 # This is a dirty hack to make a click on an open menu close it.
 # not guaranteed to work on future tk versions
-bind Menu <Button> {
-    if {$tk::Priv(menuBar) ne {}} {
-        tk::MenuEscape %W
-        set Priv(menuBar) {}
-        set Priv(focus) {}
-        unset -nocomplain Priv(menuActivated)
-        %W activate none
-    } else {
-        tk::MenuButtonDown %W
-    }
-}
+
+# bind Menu <Button> {
+#     if {$tk::Priv(menuBar) ne {}} {
+#         tk::MenuEscape %W
+#         set Priv(menuBar) {}
+#         set Priv(focus) {}
+#         unset -nocomplain Priv(menuActivated)
+#         %W activate none
+#     } else {
+#         tk::MenuButtonDown %W
+#     }
+# }
 
 set OS [lindex $tcl_platform(os) 0]
 set env(LC_ALL) C
