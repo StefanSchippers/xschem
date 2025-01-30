6a869a0078979d37dc335e9fa4efa35e8e814fa1
stefan schippers <stefan.schippers@gmail.com>
2025-01-30 03:29:13 +0100

    reduce calls to find_closest_net_or_symbol_pin() in draw_crosshair()

--- src/xschem.h
+++ src/xschem.h
@@ -1050,10 +1050,8 @@ typedef struct {
   double xpan,ypan,xpan2,ypan2;
   double p_xx1,p_xx2,p_yy1,p_yy2;
   /* draw_crosshair */
-  double prev_crossx, prev_crossy;
-  double prev_gridx, prev_gridy;
-  double prev_snapx, prev_snapy;
-  int closest_pin_found;
+  double prev_crossx, prev_crossy; /* previous closest net/pin found by draw_crosshair() */
+  double prev_m_crossx, prev_m_crossy; /* previous snap mouse position processed by draw_crosshair() */
   int mouse_inside;
   /* set_modify */
   int prev_set_modify;
@@ -1395,7 +1393,6 @@ extern void tclmainloop(void);
 extern int Tcl_AppInit(Tcl_Interp *interp);
 extern void abort_operation(void);
 extern void draw_crosshair(int what, int state);
-extern void draw_snap_cursor(int what);
 extern void backannotate_at_cursor_b_pos(xRect *r, Graph_ctx *gr);
 /* extern void snapped_wire(double c_snap); */
 extern int callback(const char *winpath, int event, int mx, int my, KeySym key,
