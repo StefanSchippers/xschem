e471f577fa1f3745c9c65821d8082baab5a531e0
stefan schippers <stefan.schippers@gmail.com>
2025-02-01 00:03:12 +0100

    integrated create_symbol.tcl in xschem.tcl, some old version strings updated in scripts, some code indenting fixes

--- src/xschem.tcl
+++ src/xschem.tcl
@@ -4874,6 +4874,46 @@ proc add_lab_prefix {} {
   xschem merge $USER_CONF_DIR/.clipboard.sch
 }
 
+# Create an xschem symbol from a list of in, out, inout pins.
+# Example:
+#   create_symbol test.sym {CLK RST D} {Q QB} {VCC VSS}
+#
+proc create_symbol {name {in {}} {out {}} {inout {}}} {
+  set symname [file rootname $name].sym
+  set res [catch {open $symname {WRONLY CREAT EXCL}} fd]
+  if {$res} {puts $fd; return 0} ;# Error. Print reason and exit.
+  puts $fd {v {xschem version=3.4.6 file_version=1.2}}
+  puts $fd {K {type=subcircuit format="@name @pinlist @symname" template="name=X1"}}
+  set x -150
+  set y 0
+  foreach pin $in { ;# create all input pins on the left
+    puts $fd "B 5 [expr {$x - 2.5}] [expr {$y - 2.5}] [expr {$x + 2.5}] [expr {$y + 2.5}] {name=$pin dir=in}"
+    puts $fd "T {$pin} [expr {$x + 25}] [expr {$y - 6}] 0 0 0.2 0.2 {}"
+    puts $fd "L 4 $x $y [expr {$x + 20}] $y {}"
+    incr y 20
+  }
+  set x 150
+  set y 0
+  foreach pin $out { ;# create all out pins on the top right
+    puts $fd "B 5 [expr {$x - 2.5}] [expr {$y - 2.5}] [expr {$x + 2.5}] [expr {$y + 2.5}] {name=$pin dir=out}"
+    puts $fd "T {$pin} [expr {$x - 25}] [expr {$y - 6}] 0 1 0.2 0.2 {}"
+    puts $fd "L 4 [expr {$x - 20}] $y $x $y {}"
+    incr y 20
+  }
+  foreach pin $inout { ;# create all inout pins on the bottom right
+    puts $fd "B 5 [expr {$x - 2.5}] [expr {$y - 2.5}] [expr {$x + 2.5}] [expr {$y + 2.5}] {name=$pin dir=inout}"
+    puts $fd "T {$pin} [expr {$x - 25}] [expr {$y - 6}] 0 1 0.2 0.2 {}"
+    puts $fd "L 7 [expr {$x - 20}] $y $x $y {}"
+    incr y 20
+  }
+  puts $fd "B 4 -130 -10 130 [expr {$y - 10}] {fill=0}" ;# symbol box
+  puts $fd "T {@symname} 0 [expr {($y - 20) / 2}] 0 0 0.3 0.3 {hcenter=1 vcenter=1}" ;#symbol name
+  puts $fd "T {@name} 130 -10 2 1 0.2 0.2 {}" ;# instance name
+  close $fd
+  puts "Created symbol ${symname}"
+  return 1
+}
+
 proc make_symbol {name {ask {no}} } {
   global XSCHEM_SHAREDIR symbol_width
   set name [abs_sym_path $name ]
