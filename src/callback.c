95cbd80767562cc21b0548fa3a2291a79d7758e2
stefan schippers <stefan.schippers@gmail.com>
2025-02-12 16:25:46 +0100

    merged in TheSUperChayan feature for displaying `Draw Wire` in status bar when in wiring mode.

--- src/callback.c
+++ src/callback.c
@@ -4503,6 +4503,12 @@ int wire_draw_active = (xctx->ui_state & STARTWIRE) ||
  }
 #endif
 
+ if(wire_draw_active) {
+    tclvareval(xctx->top_path, ".statusbar.10 configure -state active -text {DRAW WIRE! }", NULL);
+ } else {
+    tclvareval(xctx->top_path, ".statusbar.10 configure -state normal -text { }", NULL);
+ }
+
  tclvareval(xctx->top_path, ".statusbar.7 configure -text $netlist_type", NULL);
  tclvareval(xctx->top_path, ".statusbar.3 delete 0 end;",
                      xctx->top_path, ".statusbar.3 insert 0 $cadsnap",
