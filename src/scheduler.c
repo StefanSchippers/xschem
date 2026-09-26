fff73a8141139ecfc5049711b63f1e6636922f85
stefan schippers <stefan.schippers@gmail.com>
2026-09-26 10:14:52 +0200

    scheduler.c: moved fork_schematic to the right place

--- src/scheduler.c
+++ src/scheduler.c
@@ -504,34 +504,6 @@ int xschem(ClientData clientdata, Tcl_Interp *interp, int argc, const char * arg
       Tcl_ResetResult(interp);
     }
 
-    /* fork_schematic what sch_name
-     *   what:
-     *   1: save current schematic
-     *   2: copy current schematic
-     *   3: lookup schematic indicated in `sch_name` and switch to it
-     *   4: free data
-     *   6: free indicated sch_name
-     *   5: get info */
-    else if(!strcmp(argv[1], "fork_schematic"))
-    {
-      char *sch_name = NULL;
-      int what = 0;
-      int ret = 0;
-      if(!xctx) {Tcl_SetResult(interp, not_avail, TCL_STATIC); return TCL_ERROR;}
-      my_strdup2(_ALLOC_ID_, &sch_name, xctx->current_name);
-      if(argc < 3) {
-        Tcl_SetResult(interp, "xschem fork_schematic: missing arguments.", TCL_STATIC);
-        return TCL_ERROR;
-      }
-      if(argc > 3) {
-        my_strdup2(_ALLOC_ID_, &sch_name, argv[3]);
-      }
-      what = atoi(argv[2]);
-      ret = fork_schematic(what, sch_name);
-      my_free(_ALLOC_ID_, &sch_name);
-      Tcl_SetResult(interp, my_itoa(ret), TCL_VOLATILE);
-    }
-
     /* case_insensitive 1|0
      *   Set case insensitive symbol lookup. Use only on case insensitive filesystems */
     else if(!strcmp(argv[1], "case_insensitive"))
@@ -1419,6 +1391,34 @@ int xschem(ClientData clientdata, Tcl_Interp *interp, int argc, const char * arg
       if(!xctx) {Tcl_SetResult(interp, not_avail, TCL_STATIC); return TCL_ERROR;}
       floaters_from_selected_inst();
       Tcl_ResetResult(interp);
+    }
+
+    /* fork_schematic what sch_name
+     *   what:
+     *   1: save current schematic
+     *   2: copy current schematic
+     *   3: lookup schematic indicated in `sch_name` and switch to it
+     *   4: free data
+     *   6: free indicated sch_name
+     *   5: get info */
+    else if(!strcmp(argv[1], "fork_schematic"))
+    {
+      char *sch_name = NULL;
+      int what = 0;
+      int ret = 0;
+      if(!xctx) {Tcl_SetResult(interp, not_avail, TCL_STATIC); return TCL_ERROR;}
+      my_strdup2(_ALLOC_ID_, &sch_name, xctx->current_name);
+      if(argc < 3) {
+        Tcl_SetResult(interp, "xschem fork_schematic: missing arguments.", TCL_STATIC);
+        return TCL_ERROR;
+      }
+      if(argc > 3) {
+        my_strdup2(_ALLOC_ID_, &sch_name, argv[3]);
+      }
+      what = atoi(argv[2]);
+      ret = fork_schematic(what, sch_name);
+      my_free(_ALLOC_ID_, &sch_name);
+      Tcl_SetResult(interp, my_itoa(ret), TCL_VOLATILE);
     }
 
     /* fullscreen
